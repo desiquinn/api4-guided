293c341e4ac355d54d7106c32d3e06f5df42918e
Desiree Morris <desiquinncompany@gmail.com>
2019-09-05 12:44:21 -0400

    adding dotenv module

--- data/db.js
+++ data/db.js
@@ -1,4 +1,5 @@
 const knex = require('knex');
+const dotenv = require('dotenv').config()
 
 const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile.js')[environment];
